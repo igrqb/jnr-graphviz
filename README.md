602ca3bd3aa56c7deb4bd5285dfa16df5e1bc87a
igrqb <100824876+igrqb@users.noreply.github.com>
2023-10-12 13:49:37 +1000

    using batik and twelvemonkeys to generate test. direct comparison not working due to minute rendering differences

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # jnr-graphviz
-Java bindings for simple Graphviz commands using JNR
+### Java bindings for simple Graphviz commands using JNR
+
+Expects Graphviz to be installed on the target system, e.g.:
+
+* Ubuntu: `sudo apt install graphviz libgraphviz-dev`
+* Fedora: `sudo dnf in graphviz graphviz-devel`
+* etc.
---
+++ src/test/java/TestDotToSvg.java
@@ -0,0 +1,54 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
+
+public class TestDotToSvg {
+  private static final Logger log = LoggerFactory.getLogger(TestDotToSvg.class);
+
+  @Test
+  public void testSimpleDotToSvg() throws IOException {
+    String dot = "digraph { a -> b; b -> c }";
+
+    String svg = Graphviz.dotToSvg(dot);
+
+    BufferedImage expected = ImageIO.read(Objects.requireNonNull(this.getClass().getResourceAsStream("expected.svg")));
+    BufferedImage actual = ImageIO.read(new ByteArrayInputStream(svg.getBytes(StandardCharsets.UTF_8)));
+
+    Assertions.assertEquals(expected.getHeight(), actual.getHeight(), "Image heights differ");
+    Assertions.assertEquals(expected.getWidth(), actual.getWidth(), "Image widths differ");
+
+    // Image differences indicate a slight vertical shift
+    int mismatches = 0;
+    for (int x = 0; x < expected.getWidth(); x++) {
+      for (int y = 2; y < expected.getHeight() - 2; y++) {
+        if (expected.getRGB(x, y) != actual.getRGB(x, y)) {
+          log.warn("Image values differ at point ({}, {}) : expected = {}, actual = {}", x, y,
+              String.format("%x", expected.getRGB(x, y)),
+              String.format("%x", actual.getRGB(x, y)));
+          mismatches++;
+          if (expected.getRGB(x, y+2) == actual.getRGB(x, y)) {
+            log.info("Image values MATCH at point ({}, {}) : expected = {}, actual = {}", x, y,
+                String.format("%x", expected.getRGB(x, y+2)),
+                String.format("%x", actual.getRGB(x, y)));
+            mismatches--;
+          }
+        }
+      }
+    }
+
+    boolean e = ImageIO.write(expected, "bmp", new File("/tmp/expected.bmp"));
+    boolean a = ImageIO.write(actual, "bmp", new File("/tmp/actual.bmp"));
+
+    Assertions.assertEquals(0, mismatches, "Found %d mismatches".formatted(mismatches));
+  }
+}
